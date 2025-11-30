b1016a0a57825054fd529b38998b576d39a42a0f
abouttired <eden.f.ferreira@gmail.com>
2025-11-30 20:07:35 +0000

    Update index.py

--- index.py
+++ index.py
@@ -30,9 +30,9 @@ def send_morse_code():
     try:
         last_log_done = cur.execute("SELECT l_space FROM mctt.view_log WHERE l_id = (SELECT MAX(l_id) FROM mctt.view_log)")
         if last_log_done == True:
-            cur.execute("call mctt.insert_log(%s, %s)",[data["l_morse_code"], data["l_space"]])
+            cur.execute("call mctt.insert_log(%s, %s)",[data["l_morse_code"], int(data["l_space"])])
         else:
-            cur.execute("call mctt.update_log(%s, %s)",[data["l_morse_code"], data["l_space"]])
+            cur.execute("call mctt.update_log(%s, %s)",[data["l_morse_code"], int(data["l_space"])])
         conn.commit()
     except Exception as e:
         msg = {"exception": str(e)}
@@ -58,7 +58,6 @@ def get_translation():
         "l_translation": entry[3],
         "l_space": entry[4]
     }
-    
     cur.close()
     conn.close()
 
@@ -91,6 +90,7 @@ def get_logs():
 if __name__ == "__main__":
 
     app.run()
+
 
 
 
